a36b28f65aa51d7739b77bb407b3d64baa61bb57
Munson77 <65086935+Munson77@users.noreply.github.com>
2020-05-29 12:06:08 -0400

    intial push

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinchControl
+{
+    //************************************
+    //Title: Finch Control
+    //Application Type: Console
+    //Description: (describe the purpose and function)
+    //Author: Victoria Munson
+    //Date Created: 5/25/2020
+    //Last Modified:
+    //************************************
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
