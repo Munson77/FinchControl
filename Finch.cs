4d6ee6a3036b652fe27c2a140b3b88a8dc30dbc3
Munson77 <65086935+Munson77@users.noreply.github.com>
2020-05-31 22:16:44 -0400

    Finch

---
+++ Finch.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace FinchControl
+{
+    internal class Finch
+    {
+        internal void noteOn(int v)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal object setLED(int lightSoundLevel1, int lightSoundLevel2, int lightSoundLevel3)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -17,25 +17,195 @@ namespace FinchControl
     //************************************
     class Program
     {
+
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
+
+            DisplayWelcomeScreen();
+            DisplayMenuScreen();
+            DisplayClosingScreen();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.BackgroundColor = ConsoleColor.White;
+        }
+
+        private static void DisplayClosingScreen()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void DisplayWelcomeScreen()
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+        //Main Menu
+
+        static void DisplayMenuScreen()
+        {
+            Console.CursorVisible = true;
+
+            bool quitApplication = false;
+            string menuChoice;
+
+            Finch finchRobot = new Finch();
+
+            do
             {
-            Method: void DisplayHeader(string headerText);
+                DisplayScreenHeader("Main Menu");
 
-                    Console.Clear();
-                    DisplayHeader;
 
-                }
+                // get user menu choice
+
+                Console.WriteLine("\ta) Connect Finch Robot");
+                Console.WriteLine("\tb) Talent Show");
+
+                menuChoice = Console.ReadLine().ToLower();
+
+                // process user menu choice
+
+                switch (menuChoice)
                 {
-            Method: void DisplayWelcomeScreen();
+                    case "a":
+                        DisplayConnectFinchRobot(finchRobot);
+                        break;
 
+                    case "b":
+                        DisplayTalentShowMenuScreen(finchRobot);
+                        break;
 
-                    Console.Clear();
-                    Console.WriteLine("Welcome.");
-                Console.WriteLine("The purpose of this application is to");
+                    case "c":
 
+                        break;
 
+                    case "d":
+
+                        break;
+
+                    case "e":
+
+                        break;
+
+                    case "f":
+                        DisplayDisconnectFinchRobot(finchRobot);
+                        break;
+
+                    case "q":
+                        DisplayDisconnectFinchRobot(finchRobot);
+                        quitApplication = true;
+                        break;
+
+                    default:
+                        Console.WriteLine();
+                        Console.WriteLine("\tPlease enter a letter for the menu choice.");
+                        DisplayContinuePrompt();
+                        break;
+                }
+
+            } while (!quitApplication);
+        }
+
+        private static void DisplayConnectFinchRobot(Finch finchRobot)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void DisplayDisconnectFinchRobot(Finch finchRobot)
+        {
+            if (finchRobot is null)
+            {
+                throw new ArgumentNullException(nameof(finchRobot));
             }
+
+            throw new NotImplementedException();
+        }
+
+        private static void DisplayContinuePrompt()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void DisplayScreenHeader(string v)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        //Talent Show Menu                          
+
+        static void DisplayTalentShowMenuScreen(Finch myFinch)
+        {
+            Console.CursorVisible = true;
+            bool quitTalentShowMenu = false;
+            string menuChoice;
+            do
+            {
+                DisplayScreenHeader("Talent Show Menu");
+                // get user menu choice
+                Console.WriteLine("\ta) Light and Sound");
+                Console.WriteLine("\tb) ");
+                Console.WriteLine("\tc) ");
+                Console.WriteLine("\td) ");
+                Console.WriteLine("\tq) Main Menu");
+                Console.Write("\t\tEnter Choice:");
+                menuChoice = Console.ReadLine().ToLower();
+
+                switch (menuChoice)
+                {
+                    case "a":
+                        DisplayLightAndSound(myFinch);
+                        break;
+
+                    case "b":
+
+                        break;
+
+                    case "c":
+
+                        break;
+
+                    case "d":
+
+                        break;
+
+                    case "q":
+                        quitTalentShowMenu = true;
+                        break;
+
+                    default:
+                        Console.WriteLine();
+                        Console.WriteLine("\tPlease enter a letter for the menu choice.");
+                        DisplayContinuePrompt();
+                        break;
+                }
+
+            } while (!quitTalentShowMenu);
+        }
+
+        //Talent Show / Light and Sound                   
+
+        static void DisplayLightAndSound(Finch finchRobot)
+        {
+            Console.CursorVisible = false;
+
+            DisplayScreenHeader("Light and Sound");
+
+            Console.WriteLine("\tThe Finch robot will not show off its glowing talent!");
+            DisplayContinuePrompt();
+
+            for (int lightSoundLevel = 0; lightSoundLevel < 255; lightSoundLevel++)
+            {
+                object p = finchRobot.setLED(lightSoundLevel, lightSoundLevel, lightSoundLevel);
+                finchRobot.noteOn(lightSoundLevel * 100);
+            }
+            DisplayMenuPrompt("Talent Show Menu");
+        }
+
+        private static void DisplayMenuPrompt(string v)
+        {
+            throw new NotImplementedException();
         }
     }
 }
